f964d05a8486ab8c316af59a9627c47bf2a5471f
abechanta <abe.chanta@hotmail.com>
2021-11-26 23:13:47 +0900

    Support rotation unit.

--- engine/components/transform_component.h
+++ engine/components/transform_component.h
@@ -8,7 +8,6 @@
 #include <initializer_list>
 #include <ptree.h>
 #include <string>
-#include <unordered_map>
 
 namespace tte {
 	using namespace std;
@@ -32,15 +31,19 @@ namespace tte {
 		// public methods
 		//
 	public:
-		Transform(property_tree::ptree &node)
+		explicit Transform(property_tree::ptree &node)
 			: CList(tag),
 			translation(node, "translation", 0.f),
 			rotation(node, "rotation", 0.f),
 			scaling(node, "scaling", 1.f)
 		{
+			auto unit = node.get<string>("rotationUnit", "radian");
+			if (auto unitConv = Geometry::rotationConv.at(unit)) {
+				rotation(rotation() * unitConv);
+			}
 		}
 
-		Transform(property_tree::ptree &node, const vector3 &translation_, const vector3 &rotation_, const vector3 &scaling_)
+		explicit Transform(property_tree::ptree &node, const vector3 &translation_, const vector3 &rotation_, const vector3 &scaling_)
 			: CList(tag),
 			translation(node, "translation", translation_),
 			rotation(node, "rotation", rotation_),
@@ -52,19 +55,12 @@ namespace tte {
 		}
 
 		static Actor::Action append() {
-			// static const unordered_map<string, float> unitConv = {
-			// 	{ "radian", 1.f, },
-			// 	{ "degree", Geometry::deg2rad, },
-			// 	{ "fixed32", Geometry::fixed2rad, },
-			// };
 			return [](Actor &a) {
-				// const string rUnit = a.get<string>(key + ".rotation.unit", "degree");
-				// r *= unitConv.at(rUnit);
 				a.appendComponent(new Transform(a.props("transform")));
 			};
 		}
 
-		matrix3x4 & trs2d(matrix3x4 &m) {
+		matrix3x4 &trs2d(matrix3x4 &m) {
 			return Geometry::trs2d(m, translation(), rotation(), scaling());
 		}
 
